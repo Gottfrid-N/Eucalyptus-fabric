098c28920b8da909aa1e66984d87dd011fe4b100
Gottfrid <gottfrid@familjen-nordstrom.se>
2022-08-17 20:21:12 +0200

    Initial commit

---
+++ src/main/java/se/gottfrid/eucalyptus/mixin/EucalyptusMixin.java
@@ -0,0 +1,16 @@
+package se.gottfrid.eucalyptus.mixin;
+
+import se.gottfrid.eucalyptus.EucalyptusMain;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class EucalyptusMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		EucalyptusMain.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
